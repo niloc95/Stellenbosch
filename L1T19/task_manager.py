129873bfb3b42154034f2d88fbf843d95781b6cd
Nilo <nilo.cara@gmail.com>
2023-01-22 14:52:51 +0200

    L1T19 Update

--- L1T19/task_manager.py
+++ L1T19/task_manager.py
@@ -37,10 +37,9 @@ password = input('Please enter your password: ')
 while (username not in username_list) or \
         (password != password_list[username_list.index(username)]):
         print('''
-        ===================================================
-        \033[1m    Oh no incorrect details, please try again!\033[0m
-        ===================================================
-        ''')
+    ===================================================
+    \033[1m    Oh no incorrect details, please try again!\033[0m
+    ===================================================''')
         username = input('Please enter your username: ')
         password = input('Please enter your password: ')
 else: 
@@ -65,7 +64,7 @@ while True:
         va - View all tasks
         vm - view my task
         e - Exit
-     ''').lower()
+     =====>: ''').lower()
     
     else: 
         menu = input('''
@@ -77,11 +76,11 @@ while True:
      va - View all tasks
      vm - view my task
      e - Exit
-     ''').lower()
+     =====>: ''').lower()
      
     if menu == 's':
-        with open ('tasks.txt', 'r') as task_items:
-            list_num_task = len(task_items.readlines())
+        with open ('tasks.txt', 'r') as tasks_file:
+            list_num_task = len(tasks_file.readlines())
         
         with open ('user.txt', 'r') as users_file:
             list_num_users = len(users_file.readlines())
@@ -122,55 +121,48 @@ while True:
             print('''
         =========================================================================
              Sorry! administrator privileges are required to perform this task
-        =========================================================================
-        ''')
+        =========================================================================''')
             continue
 
     elif menu == 'a':
         print(''' 
             =======================
                 Lets add a task
-            =======================
-        ''')
+            =======================''')
         task_username = input('Task for, who must do the task ==>: ')
         while task_username not in username_list:
             print('''
             =================================================
                   Let's try again user not found!
-            =================================================
-            ''')
+            =================================================''')
             task_username = input('Task for: ')
 
         task_name = input(' Task name: ')
         task_desc = input(' Task description: ')
         task_due = input(' Task due date: ')
-        current_date = date.today().strftime("%d %b, %Y")
+        current_date = date.today().strftime("%d %b %Y")
         task_completion = "No"
 
-        if not len(task_username) or not len (task_name) or not len(task_desc) \
+        if not len(task_username) or not len(task_name) or not len(task_desc) \
                     or not len(task_due):
             print('''
         ======================================================
             Please enter all the task items, Lets try again! 
-        ======================================================
-                ''')
+        ======================================================''')
         else: 
             tasks_file = open('tasks.txt', 'a+')
-            tasks_file.write(f'\n{task_username}, {task_name}, {task_desc} '
-                                 f'{task_due}, {current_date},{task_completion}')
+            tasks_file.write(f'\n{task_username}, {task_name}, {task_desc}, {task_due}, {current_date}, {task_completion}')
             tasks_file.close()
             print('''
                 *****************************************************
                             New task successfully added
-                *****************************************************
-                ''')
+                *****************************************************''')
     
     elif menu == 'va':
        print(''' 
         *********************************************
                     Lets view all tasks
-        *********************************************
-        ''')
+        *********************************************''')
        with open ('tasks.txt', 'r') as tasks_file:
             for line in tasks_file:
                 line = line.split(', ')
@@ -182,52 +174,45 @@ while True:
                 complete_task = line[5]
 
                 print(f'''
-                -------------------------------------------------------------------------------------------------------------------------------------
-                -                                                                                                                                   
+                -------------------------------------------------------------------------------------------------------------------------------------                                                                                     
                 -   Task Item:              {task}                                                                                                  
                 -   Task Assignee:          {assignee}                                                                                              
                 -   Date Assigned:          {assigned}                                                                                              
                 -   Due Date:               {date_due}                                                                                              
                 -   Task Complete Status:   {complete_task}                                                                                         
                 -   Task Description:       {task_descsription}
-                -
-                -------------------------------------------------------------------------------------------------------------------------------------
-
-                ''')
+                -------------------------------------------------------------------------------------------------------------------------------------''')
 
     elif menu == 'vm':
         print(''' 
-        *********************************************
-                    View all tasks
-        *********************************************
-        ''')
+    *********************************************
+                View all tasks
+    *********************************************''')
+        
         with open ('tasks.txt', 'r') as tasks_file:
-                for line in tasks_file:
-                    line = line.split(', ')
-                    if username == line[0]:
-                        my_tasks = line
-                        task = line[1]
-                        task_descsription = line[2]
-                        date_due = line[3]
-                        assigned = line[4]
-                        complete_task = line[5]
+            for line in tasks_file:
+                line = line.split(', ')
+                if username == line[0]:
+                    my_tasks = line
+                    task = line[1]
+                    task_descsription = line[2]
+                    date_due = line[3]
+                    assigned = line[4]
+                    complete_task = line[5]
 
-                    print(f'''
-                -------------------------------------------------------------------------------------------------------------------------------------
-                -                                                                                                                                   
-                -   Task Item:              {task}                                                                                                                                                                                           
+                print(f'''
+                -------------------------------------------------------------------------------------------------------------------------------------                                                                                                                                 
+                -   Task Item:              {task}                                                                                                                                                                                          
                 -   Date Assigned:          {assigned}                                                                                              
                 -   Due Date:               {date_due}                                                                                              
                 -   Task Complete Status:   {complete_task}                                                                                         
                 -   Task Description:       {task_descsription}
-                - 
                 -------------------------------------------------------------------------------------------------------------------------------------''')
-                else: 
-                    print('''
+            else: 
+                print('''
             ====================================  
               No more tasks available for you
-            ====================================
-            ''')        
+            ====================================''')        
 
     elif menu == 'e':
         print('Goodbye!!!😀')
